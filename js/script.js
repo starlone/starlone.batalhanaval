95b7aabbf368b50647987f94ac7930a61f9aca1b
Starlone <starlone.passos@gmail.com>
2014-03-29 00:10:37 -0300

    script.js - reidentando

--- js/script.js
+++ js/script.js
@@ -1,64 +1,64 @@
 function paginaInicial(){
-    $("#menu").show();
-    $("#botoes").empty();              
+	$("#menu").show();
+	$("#botoes").empty();              
 }
 
 function newGame(e){
-  e.preventDefault();
-    game = new BatalhaNaval();    
-    game.startGame();    
-    adicionaBotoes();    
+	e.preventDefault();
+	game = new BatalhaNaval();    
+	game.startGame();    
+	adicionaBotoes();    
 }
 
 function adicionaBotoes(){
-  l = 7;
-  c =7; 
-  $("#menu").hide();
-  $("#botoes").empty();
-  $("#botoes").append("<table id='tabela'>");
-  for(var i = 0; i <= l; i++){
-    var linha = $("<tr>");
-    for(var j=0; j <= c; j++){
-      if(i == 0 && j!=0){       
-        var th = $("<th>"+j+"</th>");                   
-        linha.append(th);
-      }
-      else{
-        if(j==0){
-          if(i!=0){           
-            var th = $("<th>"+i+"</th>");       
-            linha.append(th);
-          }
-          else{
-            var th = $("<th>");       
-            linha.append(th);
-          }
-        }
-        else{
-          var coluna = $("<td class = 'botao' id = '"+i+""+j+"'>"+i+""+j+"</td>");                    
-          linha.append(coluna);
-        }
-      }
-    }       
-    $("#tabela").append(linha);
-  }
-  $(".botao").click(verificaGuess);
+	l = 7;
+	c =7;
+	$("#menu").hide();
+	$("#botoes").empty();
+	$("#botoes").append("<table id='tabela' class='table'>");
+	for(var i = 0; i <= l; i++){
+		var linha = $("<tr>");
+		for(var j=0; j <= c; j++){
+			if(i == 0 && j!=0){
+				var th = $("<th>"+j+"</th>");
+				linha.append(th);
+			}
+			else{
+				if(j==0){
+					if(i!=0){
+						var th = $("<th>"+i+"</th>");
+						linha.append(th);
+					}
+					else{
+						var th = $("<th>");
+						linha.append(th);
+					}
+				}
+				else{
+					var coluna = $("<td class = 'botao' id = '"+i+""+j+"'>"+i+""+j+"</td>");
+					linha.append(coluna);
+				}
+			}
+		}
+		$("#tabela").append(linha);
+	}
+	$(".botao").click(verificaGuess);
 }
 
 function verificaGuess(){
-  flag = game.checkGuess($(this).html());
-  if(flag){
-    $(this).css("background-color","yellow");
-  }
-  if(game.endGame()){
-      alert("Você ganhou");
-      paginaInicial();
-  }
-  $(this).removeClass("botao");
-  $(this).addClass("botao_pressed");
+	flag = game.checkGuess($(this).html());
+	if(flag){
+		$(this).css("background-color","yellow");
+	}
+	if(game.endGame()){
+		alert("Você ganhou");
+		paginaInicial();
+	}
+	$(this).removeClass("botao");
+	$(this).addClass("botao_pressed");
 }
 
 $(document).ready(function(){ 
-  $(".newgame").click(newGame);              
-  $("#home").click(paginaInicial);
+	$(".newgame").click(newGame);              
+	$("#home").click(paginaInicial);
 });
